a897286e0dbccad530a4978d29180379ccafab4b
Carter Eikenbary <eikenbcr@Carters-MacBook-Pro.local>
2020-05-23 12:36:48 -0400

    Working Code

--- New2017/ChainedStatistics.C
+++ New2017/ChainedStatistics.C
@@ -296,60 +296,127 @@ void ChainedStatistics::Terminate()
       TString totalStr;
       TString deltaTotalStr;
 
-      TF1 *SingleGaussian = new TF1("SingleGaussian",fit2MeV_Gaussian,2100.,2500.,5);
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
+      TF1 *myLambdaFit = new TF1("myLambdaFit",fit2MeV_Gaussian,2100.,2500.,5);
+      myLambdaFit->SetParameter(0,400.);
+      myLambdaFit->SetParameter(1,2286.5);
+      myLambdaFit->SetParameter(2, 6);
+      myLambdaFit->SetParLimits(2, 0.,20.);
+      myLambdaFit->SetParameter(3, 0.);
+      myLambdaFit->SetParameter(4, 0.);
 
+      TF1 *LcFit1 = new TF1("LcFit1",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFit1->SetParameter(0,400.);
+      LcFit1->SetParameter(1,2286.5);
+      LcFit1->SetParameter(2, 6);
+      LcFit1->SetParLimits(2, 0.,20.);
+      LcFit1->SetParameter(3, 0.);
+      LcFit1->SetParameter(4, 0.);
 
-      OptimalCut->Fit("SingleGaussian");
+      TF1 *LcFit2 = new TF1("LcFit2",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFit2->SetParameter(0,400.);
+      LcFit2->SetParameter(1,2286.5);
+      LcFit2->SetParameter(2, 6);
+      LcFit2->SetParLimits(2, 0.,20.);
+      LcFit2->SetParameter(3, 0.);
+      LcFit2->SetParameter(4, 0.);
+
+      TF1 *LcFit3 = new TF1("LcFit3",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFit3->SetParameter(0,400.);
+      LcFit3->SetParameter(1,2286.5);
+      LcFit3->SetParameter(2, 6);
+      LcFit3->SetParLimits(2, 0.,20.);
+      LcFit3->SetParameter(3, 0.);
+      LcFit3->SetParameter(4, 0.);
+
+      TF1 *LcFit4 = new TF1("LcFit4",fit2MeV_Gaussian,2100.,2500.,5);
+      LcFit4->SetParameter(0,400.);
+      LcFit4->SetParameter(1,2286.5);
+      LcFit4->SetParameter(2, 6);
+      LcFit4->SetParLimits(2, 0.,20.);
+      LcFit4->SetParameter(3, 0.);
+      LcFit4->SetParameter(4, 0.);
+
+      TF1 *XiFit1 = new TF1("XiFit1",fit2MeV_Gaussian,2100.,2500.,5);
+      XiFit1->SetParameter(0,400.);
+      XiFit1->SetParameter(1,2286.5);
+      XiFit1->SetParameter(2, 6);
+      XiFit1->SetParLimits(2, 0.,20.);
+      XiFit1->SetParameter(3, 0.);
+      XiFit1->SetParameter(4, 0.);
+
+      TF1 *XiFit2 = new TF1("XiFit2",fit2MeV_Gaussian,2100.,2500.,5);
+      XiFit2->SetParameter(0,400.);
+      XiFit2->SetParameter(1,2286.5);
+      XiFit2->SetParameter(2, 6);
+      XiFit2->SetParLimits(2, 0.,20.);
+      XiFit2->SetParameter(3, 0.);
+      XiFit2->SetParameter(4, 0.);
+
+      TF1 *XiFit3 = new TF1("XiFit3",fit2MeV_Gaussian,2100.,2500.,5);
+      XiFit3->SetParameter(0,400.);
+      XiFit3->SetParameter(1,2286.5);
+      XiFit3->SetParameter(2, 6);
+      XiFit3->SetParLimits(2, 0.,20.);
+      XiFit3->SetParameter(3, 0.);
+      XiFit3->SetParameter(4, 0.);
+
+      TF1 *XiFit4 = new TF1("XiFit4",fit2MeV_Gaussian,2100.,2500.,5);
+      XiFit4->SetParameter(0,400.);
+      XiFit4->SetParameter(1,2286.5);
+      XiFit4->SetParameter(2, 6);
+      XiFit4->SetParLimits(2, 0.,20.);
+      XiFit4->SetParameter(3, 0.);
+      XiFit4->SetParameter(4, 0.);
+
+      TF1 *MagDownFit = new TF1("MagDownFit",fit2MeV_Gaussian,2100.,2500.,5);
+      MagDownFit->SetParameter(0,400.);
+      MagDownFit->SetParameter(1,2286.5);
+      MagDownFit->SetParameter(2, 6);
+      MagDownFit->SetParLimits(2, 0.,20.);
+      MagDownFit->SetParameter(3, 0.);
+      MagDownFit->SetParameter(4, 0.);
+
+      TF1 *MagUpFit = new TF1("MagUpFit",fit2MeV_Gaussian,2100.,2500.,5);
+      MagUpFit->SetParameter(0,400.);
+      MagUpFit->SetParameter(1,2286.5);
+      MagUpFit->SetParameter(2, 6);
+      MagUpFit->SetParLimits(2, 0.,20.);
+      MagUpFit->SetParameter(3, 0.);
+      MagUpFit->SetParameter(4, 0.);
+
+      TF1 *ParticleFit = new TF1("ParticleFit",fit2MeV_Gaussian,2100.,2500.,5);
+      ParticleFit->SetParameter(0,400.);
+      ParticleFit->SetParameter(1,2286.5);
+      ParticleFit->SetParameter(2, 6);
+      ParticleFit->SetParLimits(2, 0.,20.);
+      ParticleFit->SetParameter(3, 0.);
+      ParticleFit->SetParameter(4, 0.);
+
+      TF1 *AntiParticleFit = new TF1("AntiParticleFit",fit2MeV_Gaussian,2100.,2500.,5);
+      AntiParticleFit->SetParameter(0,400.);
+      AntiParticleFit->SetParameter(1,2286.5);
+      AntiParticleFit->SetParameter(2, 6);
+      AntiParticleFit->SetParLimits(2, 0.,20.);
+      AntiParticleFit->SetParameter(3, 0.);
+      AntiParticleFit->SetParameter(4, 0.);
+
+      OptimalCut->Fit("myLambdaFit");
       OptimalCut->SetMinimum(0);
       c1->Write("OptimalCut");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      LcLowP->Fit("SingleGaussian");
+      LcLowP->Fit("LcFit1");
       LcLowP->SetMinimum(0);
       c1->Write("LcPLow");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      LcMidLowP->Fit("SingleGaussian");
+      LcMidLowP->Fit("LcFit2");
       LcMidLowP->SetMinimum(0);
       c1->Write("LcPMidLow");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      LcMidHighP->Fit("SingleGaussian");
+      LcMidHighP->Fit("LcFit3");
       LcMidHighP->SetMinimum(0);
       c1->Write("LcPMidHigh");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      LcHighP->Fit("SingleGaussian");
+      LcHighP->Fit("LcFit4");
       LcHighP->SetMinimum(0);
       c1->Write("LcPHigh");
 
@@ -385,47 +452,19 @@ void ChainedStatistics::Terminate()
       gLcP->Draw("ALP");
       c1->Write("LcPDivision");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      XiLowP->Fit("SingleGaussian");
+      XiLowP->Fit("XiFit1");
       XiLowP->SetMinimum(0);
       c1->Write("XiPLow");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      XiMidLowP->Fit("SingleGaussian");
+      XiMidLowP->Fit("XiFit2");
       XiMidLowP->SetMinimum(0);
       c1->Write("XiPMidLow");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      XiMidHighP->Fit("SingleGaussian");
+      XiMidHighP->Fit("XiFit3");
       XiMidHighP->SetMinimum(0);
       c1->Write("XiPMidHigh");
 
-      SingleGaussian->SetParameter(0,2000.);
-      SingleGaussian->SetParameter(1,2287);
-      SingleGaussian->SetParameter(2, 6);
-      SingleGaussian->SetParLimits(2, 0.,20.);
-      SingleGaussian->SetParameter(3, 0.);
-      SingleGaussian->SetParameter(4, 0.);
-
-      XiHighP->Fit("SingleGaussian");
+      XiHighP->Fit("XiFit4");
       XiHighP->SetMinimum(0);
       c1->Write("XiPHigh");
 
@@ -457,25 +496,11 @@ void ChainedStatistics::Terminate()
     gXiP->Draw("ALP");
     c1->Write("XiPDivision");
 
-    SingleGaussian->SetParameter(0,2000.);
-    SingleGaussian->SetParameter(1,2287);
-    SingleGaussian->SetParameter(2, 6);
-    SingleGaussian->SetParLimits(2, 0.,20.);
-    SingleGaussian->SetParameter(3, 0.);
-    SingleGaussian->SetParameter(4, 0.);
-
-    PolarityMagDown->Fit("SingleGaussian");
+    PolarityMagDown->Fit("MagDownFit");
     PolarityMagDown->SetMinimum(0);
     c1->Write("PolarityMagDown");
 
-    SingleGaussian->SetParameter(0,2000.);
-    SingleGaussian->SetParameter(1,2287);
-    SingleGaussian->SetParameter(2, 6);
-    SingleGaussian->SetParLimits(2, 0.,20.);
-    SingleGaussian->SetParameter(3, 0.);
-    SingleGaussian->SetParameter(4, 0.);
-
-    PolarityMagUp->Fit("SingleGaussian");
+    PolarityMagUp->Fit("MagUpFit");
     PolarityMagUp->SetMinimum(0);
     c1->Write("PolarityMagUp");
 
@@ -501,25 +526,11 @@ void ChainedStatistics::Terminate()
     gPolarity->Draw("ALP");
     c1->Write("PolarityDivision");
 
-    SingleGaussian->SetParameter(0,2000.);
-    SingleGaussian->SetParameter(1,2287);
-    SingleGaussian->SetParameter(2, 6);
-    SingleGaussian->SetParLimits(2, 0.,20.);
-    SingleGaussian->SetParameter(3, 0.);
-    SingleGaussian->SetParameter(4, 0.);
-
-    Particle->Fit("SingleGaussian");
+    Particle->Fit("ParticleFit");
     Particle->SetMinimum(0);
     c1->Write("Baryon");
 
-    SingleGaussian->SetParameter(0,2000.);
-    SingleGaussian->SetParameter(1,2287);
-    SingleGaussian->SetParameter(2, 6);
-    SingleGaussian->SetParLimits(2, 0.,20.);
-    SingleGaussian->SetParameter(3, 0.);
-    SingleGaussian->SetParameter(4, 0.);
-
-    AntiParticle->Fit("SingleGaussian");
+    AntiParticle->Fit("AntiParticleFit");
     AntiParticle->SetMinimum(0);
     c1->Write("AntiBaryon");
 
